886b2946e43a1b12328e1b62b4cacb8617e53473
Grishny <grishyakova00@gmail.com>
2023-09-10 13:30:06 +0400

    added test v1 exemples

---
+++ src/test/java/exemples/JUnit5Exemples.java
@@ -0,0 +1,17 @@
+package exemples;
+
+import org.junit.jupiter.api.Test;
+
+public class JUnit5Exemples {
+
+    @Test
+    void firstTest1() {
+        System.out.println("firstTest1");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("secondTest");
+    }
+}
+
