251bf5a46556fd62d608763c5d7d116680421e66
Grishny <grishyakova00@gmail.com>
2023-09-10 13:45:35 +0400

    added after and before for exemples

--- src/test/java/exemples/JUnit5Exemples.java
+++ src/test/java/exemples/JUnit5Exemples.java
@@ -1,17 +1,37 @@
 package exemples;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class JUnit5Exemples {
+    @BeforeAll
+    static void setup(){
+        System.out.println("setup");
+    }
+
+    @BeforeEach
+    void beforeEach (){
+        System.out.println("beforeEach");
+    }
+
 
     @Test
-    void firstTest1() {
-        System.out.println("firstTest1");
+    void firstTest() {
+        System.out.println("____firstTest");
     }
 
     @Test
     void secondTest() {
-        System.out.println("secondTest");
+        System.out.println("____secondTest");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("afterEach");
+    }
+
+    @AfterAll
+    static void tearDown(){
+        System.out.println("tearDown");
     }
 }
 
